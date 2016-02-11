6b677558233f3def0a5fde399f1da5c4121104ce
Russell Wright <rbwright@us.ibm.com>
2016-02-11 18:00:37 +0000

    Fixup LCB_DATABASE_NAME and LCB_DATABASE_URI.

--- lets-chat/scripts/start-server.sh
+++ lets-chat/scripts/start-server.sh
@@ -29,10 +29,11 @@ echo "running start-server.sh script"
 
 #Check for a database name passed in via --env, otherwise use default database name below
 #DEFAULT_DBNAME is "lets-chat-mongo"
-export LCB_DATABASE_NAME=${LCB_DATABASE_NAME:-lets-chat-mongo}
+#export LCB_DATABASE_NAME=${LCB_DATABASE_NAME:-lets-chat-mongo}
+export LCB_DATABASE_NAME=mongodb-lets-chat
 
 # export LCB_DATABASE_URI="mongodb://${TEMP_USER}:${TEMP_PASS}@${TEMP_URI}:${TEMP_PORT}/${LCB_DATABASE_NAME}"
-export LCB_DATABASE_URI="mongodb://031760db-a3b0-4536-ad94-2cf37cb77ea0:07f26653-f2e9-4e30-b38b-08c0ea1cdd08@mongodb://031760db-a3b0-4536-ad94-2cf37cb77ea0:07f26653-f2e9-4e30-b38b-08c0ea1cdd08@50.23.230.151:10311/db:10311/mongodb-lets-chat"
+export LCB_DATABASE_URI="mongodb://031760db-a3b0-4536-ad94-2cf37cb77ea0:07f26653-f2e9-4e30-b38b-08c0ea1cdd08@50.23.230.151:10311/mongodb-lets-chat"
 
 # Inserting sleep delay per recommendation via https://www.ng.bluemix.net/docs/containers/container_single_ov.html#container_single_cli
 sleep 60
